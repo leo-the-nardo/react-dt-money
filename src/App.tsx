d78b52ef6e14629beabde8391f2ba34450bd1697
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-11-18 19:58:48 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default.ts";
+import { GlobalStyle } from "./styles/global.ts";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Opa</h1>
+    </ThemeProvider>
+  );
+}
