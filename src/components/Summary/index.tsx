db2e3bfe7665a87033ea059071a67e2ae58528d0
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-11-18 22:00:28 -0300

    feat: summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import { SummaryCard, SummaryContainer } from "./styles.ts"
+import {
+  ArrowCircleDown,
+  ArrowCircleUp,
+  CurrencyDollar,
+  Money,
+} from "@phosphor-icons/react"
+import { useTheme } from "styled-components"
+
+export function Summary() {
+  const theme = useTheme()
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <p>Entradas</p>
+          <ArrowCircleUp size={"2rem"} color={theme["green-500"]} />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <p>Saídas</p>
+          <ArrowCircleDown size={"2rem"} color={theme["red-300"]} />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <p>Total</p>
+          <CurrencyDollar size={"2rem"} color={theme["gray-100"]} />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,51 @@
+import styled, { css } from "styled-components"
+
+export const SummaryContainer = styled.section`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 1120px;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: 2.5rem;
+`
+
+type SummaryCardProps = {
+  variant?: "green"
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  display: flex;
+  padding: 1.5rem 1.5rem 1.5rem 2rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.75rem;
+
+  background-color: ${(props) => props.theme["gray-700"]};
+  border-radius: 6px;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    p {
+      color: ${(props) => props.theme["gray-300"]};
+    }
+  }
+
+  strong {
+    color: ${(props) => props.theme["gray-100"]};
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 140%; /* 2.8rem */
+  }
+
+  ${(props) =>
+    props.variant === "green" &&
+    css`
+      background-color: ${props.theme["green-700"]};
+    `}
+`
